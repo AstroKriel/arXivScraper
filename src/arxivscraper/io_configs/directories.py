7dc470f06b58b23574fae41ee98ec4df1283ad27
neco kriel <necokriel@gmail.com>
2025-09-15 10:24:58 +1000

    better output dir names

--- src/arxivscraper/io_configs/directories.py
+++ src/arxivscraper/io_configs/directories.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).resolve().parents[3]
-search_configs = PROJECT_ROOT / "search_configs"
-output_mdfiles = PROJECT_ROOT / "output_mdfiles"
-output_pdfs    = PROJECT_ROOT / "output_pdfs"
+search_configs = PROJECT_ROOT / "configs"
+output_mdfiles = PROJECT_ROOT / "mdfiles"
+output_pdfs    = PROJECT_ROOT / "pdfs"
 
 ## .
